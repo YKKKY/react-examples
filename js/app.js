048aac0c595217ae680a67f465adaf05d77d01d3
YKKKY <1131818177@qq.com>
2016-08-04 17:19:46 +0800

    test

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const App= React.createClass({
+    getInitialState: function () {
+        return {
+            count: 0
+        }
+    },
+    add:function () {
+        this.setState({count: this.state.count + 1});
+    },
+    render:function() {
+        return <div>
+            <Count count={this.state.count} addC={this.add}/>
+        </div>
+    }
+});
+
+const Count = React.createClass({
+    adds:function(){
+        this.props.addC();
+    },
+    render:function() {
+        return <div>
+            {this.props.count}
+            <button onClick={this.adds}>+</button>
+        </div>
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('content'));
+
